faa9783280080f812f65736af19fb8806e5139e4
qw3rty-dev <rishabh120805@gmail.com>
2026-04-05 17:17:23 +0530

    Refactor job menu and add insights feature

--- cli.py
+++ cli.py
@@ -1,57 +1,81 @@
 from scrap import jobs_scraper
 from manager import printjobs,view_job,search_job,apply_job,filter_jobs
-from utils import num_input,load_json,update_json
+from utils import num_input,load_json,clear
+from analysis import insights,json_to_df
 
-def menu():
+
+def menu(total_jobs):
    
    print("\n\n"+"="*40 )
    print("JobFlow".center(40))
    print("="*40)
-   
+   print(f"\nTotal jobs loaded: {total_jobs}")
+   print("-"*40)
 
-   print(f"[1] Scrape jobs\n"
+   print(f"[1] Refresh jobs\n"
        "[2] Show all jobs\n"
        "[3] Search jobs\n"
        "[4] View job(detail view)\n"
        "[5] Apply job\n"
-       "[6] Filter jobs\n"
-       "[7] Exit\n")
+       "[6] Filter jobs\n"\
+       "[7] Insights\n"\
+       "[8] Exit\n")
    while True:
         choice = num_input("Enter your choice: ")
         if choice is None:
             continue
-        if 1 <= choice <= 7:
+        if 1 <= choice <= 8:
             return choice
-        print("Choose between 1-7")
+        print("Choose between 1-8")
 
 def main():
     while True:
-        jobsList=load_json()
-        choice=menu()
+        clear()
+        job_list=load_json("jobs.json")
+        total_jobs=len(job_list)
+
+        choice=menu(total_jobs)
         if choice==1:
             print("Scraping....")
-            jobs_scraper(jobsList)
+            jobs_scraper(job_list)
 
         elif choice==2:
-            printjobs(jobsList)
+            printjobs(job_list)
+            input("\nPress enter to return to menu...")
+
 
         elif choice==3:
             keywords=input("Search: ")
-            search_job(keywords,jobsList)
+            search_job(keywords,job_list)
+            input("\nPress enter to return to menu...")
+
 
         elif choice==4:
             job_id=num_input("Enter Job Id: ")
-            view_job(job_id)      
+            view_job(job_list,job_id)   
+            input("\nPress enter to return to menu...")
+   
 
         elif choice==5:
             job_id=num_input("Enter Job Id: ")
-            apply_job(job_id,jobsList) 
+            apply_job(job_list,job_id) 
+            input("\nPress enter to return to menu...")
+
 
         elif choice==6:
-            filter_jobs(jobsList)
+            filter_jobs(job_list)
+            input("\nPress enter to return to menu...")
+
 
         elif choice==7:
+            df=json_to_df("jobs.json")
+            insights(df)
+            input("\nPress enter to return to menu...")
+
+
+        elif choice==8:
             break
+
 
 
 if __name__=="__main__":
